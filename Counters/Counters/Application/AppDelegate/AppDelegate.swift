46b1866da04943ef453e2763fee65e8dbaf90ab5
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-03 11:50:16 -0400

    Add AppCoordinator & modify the behavior of start

--- Counters/Counters/Application/AppDelegate/AppDelegate.swift
+++ Counters/Counters/Application/AppDelegate/AppDelegate.swift
@@ -8,16 +8,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var appCoordinator: AppCoordinator?
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
         let window = UIWindow()
-        let presenter = WelcomeViewPresenter()
-        window.rootViewController = WelcomeViewController(presenter: presenter)
-        self.window = window
-        window.makeKeyAndVisible()
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start()
         return true
     }
 }
