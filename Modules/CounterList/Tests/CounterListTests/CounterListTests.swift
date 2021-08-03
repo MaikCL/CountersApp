b21af43958e35b3f6543a0ed9a8fccc8e629f248
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-02 20:51:43 -0400

    Add Empty Packages for the new Modules
    - Added Module Design System (for reusable UI Components like Colors, Fonts, Views)
    - Added feature module CounterList
    - Added feature module NewCounter

---
+++ Modules/CounterList/Tests/CounterListTests/CounterListTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import CounterList
+
+    final class CounterListTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(CounterList().text, "Hello, World!")
+        }
+    }
