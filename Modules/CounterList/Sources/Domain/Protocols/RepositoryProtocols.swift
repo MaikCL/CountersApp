b7280e1df240673ca6bf0e2b881ef559849f17e4
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-03 17:38:51 -0400

    Implement CounterRepo and connect the cloudsource

--- Modules/CounterList/Sources/Domain/Protocols/RepositoryProtocols.swift
+++ Modules/CounterList/Sources/Domain/Protocols/RepositoryProtocols.swift
@@ -2,7 +2,6 @@ import Combine
 
 protocol CountersRepositoryProtocol: AnyObject {
     func fetchCounters() -> AnyPublisher<[Counter], Error>
-    func createCounter(name: String) -> AnyPublisher<Counter, Error>
     func incrementCounter(id: String) -> AnyPublisher<[Counter], Error>
     func decrementCounter(id: String) -> AnyPublisher<[Counter], Error>
     func deleteCounter(id: String) -> AnyPublisher<[Counter], Error>
