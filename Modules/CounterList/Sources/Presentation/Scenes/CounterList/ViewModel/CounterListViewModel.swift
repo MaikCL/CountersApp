062fd658988d096625564b57f4ca38147b7ba309
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-03 21:06:05 -0400

    Implement CounterListViewModel
    - Implement the metod fetchCountes

--- Modules/CounterList/Sources/Presentation/Scenes/CounterList/ViewModel/CounterListViewModel.swift
+++ Modules/CounterList/Sources/Presentation/Scenes/CounterList/ViewModel/CounterListViewModel.swift
@@ -1,11 +1,49 @@
+import Combine
+import Resolver
+import Foundation
+import AltairMDKCommon
+
+struct ViewState {
+    var counters: Loadable<[CounterModel]> = .neverLoaded
+    var exception: Exception? = .none
+}
+
 protocol CounterListViewModelProtocol {
     var coordinator: CounterListFlow? { get set }
+    var statePublisher: Published<ViewState>.Publisher { get }
+    
+    func fetchCounters()
 }
 
 final class CounterListViewModel: CounterListViewModelProtocol {
+    @Injected private var counterListStore: CounterListStore
+    
+    private var cancellables = Set<AnyCancellable>()
+    @Published private var viewState = ViewState()
+    
+    var statePublisher: Published<ViewState>.Publisher { $viewState }
     var coordinator: CounterListFlow?
     
     init() {
-        
+        setupViewState()
+    }
+    
+    func fetchCounters() {
+        counterListStore.trigger(.fetchCounters)
+    }
+    
+}
+
+extension CounterListViewModel {
+    @Injected static var mapCounterToCounterModel: ([Counter]) -> [CounterModel]
+    
+    private func setupViewState() {
+        counterListStore.$state.receive(on: DispatchQueue.main).sink { [weak self] state in
+            guard let self = self else { return }
+            let counters = state.counters.map { CounterListViewModel.mapCounterToCounterModel($0) }
+            let exception = state.exception
+            self.viewState = ViewState(counters: counters, exception: exception)
+        }
+        .store(in: &cancellables)
     }
 }
