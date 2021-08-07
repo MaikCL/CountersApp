1a3030abbb6b1f4e1b562d170276b382af705bda
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-07 14:44:59 -0400

    Minnor improvements in CounterListView

--- Modules/CounterList/Sources/Presentation/Scenes/CounterList/View/CounterListView.swift
+++ Modules/CounterList/Sources/Presentation/Scenes/CounterList/View/CounterListView.swift
@@ -1,6 +1,13 @@
 import UIKit
 import Design
 
+protocol CounterListViewDelegate: AnyObject {
+    func didRefreshCounterList()
+    func didTapAddCounterButton()
+    func didTapShareCounterButton(selectedItems: [IndexPath])
+    func didTapDeleteCounterButton(selectedItems: [IndexPath])
+}
+
 final class CounterListView: UIView {
     
     lazy var collectionView: UICollectionView = {
@@ -14,6 +21,37 @@ final class CounterListView: UIView {
     lazy var refreshControl: UIRefreshControl = {
         setupRefreshControl()
     }()
+    
+    lazy var addCounterButtonItem: UIBarButtonItem = {
+        setupAddButtonItem()
+    }()
+    
+    lazy var shareCounterButtonItem: UIBarButtonItem = {
+       setupShareButtonItem()
+    }()
+    
+    lazy var deleteCounterButtonItem: UIBarButtonItem = {
+       setupDeleteButtonItem()
+    }()
+    
+    lazy var selectAllCounterButtonItem: UIBarButtonItem = {
+        setupSelectAllButtonItem()
+    }()
+    
+    var isEditMode: Bool = false {
+        didSet {
+            collectionView.isEditing = isEditMode
+            collectionView.allowsMultipleSelectionDuringEditing = isEditMode
+            collectionView.indexPathsForVisibleItems.forEach { indexPath in
+                guard let cell = collectionView.cellForItem(at: indexPath) as? CounterCellView else { return }
+                UIView.animate(withDuration: 0.3) {
+                    cell.isEditing = self.isEditMode
+                }
+            }
+        }
+    }
+    
+    var delegate: CounterListViewDelegate?
     
     init() {
         super.init(frame: .zero)
@@ -60,6 +98,43 @@ private extension CounterListView {
             collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
     }
+
+}
+
+extension CounterListView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? CounterCellView else { return }
+        cell.isEditing = isEditMode
+    }
+    
+}
+
+private extension CounterListView {
+    
+    @objc func addButtonAction(_ sender: Any) {
+        delegate?.didTapAddCounterButton()
+    }
+    
+    @objc func shareButtonAction(_ sender: Any) {
+        guard let selectedItems = collectionView.indexPathsForSelectedItems else { return }
+        delegate?.didTapShareCounterButton(selectedItems: selectedItems)
+    }
+    
+    @objc func deleteButtonAction(_ sender: Any) {
+        guard let selectedItems = collectionView.indexPathsForSelectedItems else { return }
+        delegate?.didTapDeleteCounterButton(selectedItems: selectedItems)
+    }
+    
+    @objc func selectAllButtonAction(_ sender: Any) {
+        for row in 0..<collectionView.numberOfItems(inSection: Section.main.rawValue) {
+            collectionView.selectItem(at: IndexPath(row: row, section: Section.main.rawValue), animated: false, scrollPosition: .centeredHorizontally)
+        }
+    }
+    
+    @objc func refreshCounterList(_ sender: Any) {
+        delegate?.didRefreshCounterList()
+    }
     
 }
 
@@ -69,6 +144,7 @@ private extension CounterListView {
         let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: CollectionViewLayout.layout)
         collectionView.backgroundColor = Palette.background.uiColor
         collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
         return collectionView
     }
     
@@ -81,7 +157,32 @@ private extension CounterListView {
     
     func setupRefreshControl() -> UIRefreshControl {
         let control = UIRefreshControl(frame: .zero)
+        control.addTarget(self, action: #selector(refreshCounterList(_:)), for: .valueChanged)
         return control
+    }
+    
+    func setupAddButtonItem() -> UIBarButtonItem {
+        let buttonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonAction(_:)))
+        buttonItem.tintColor = Palette.accent.uiColor
+        return buttonItem
+    }
+    
+    func setupDeleteButtonItem() -> UIBarButtonItem {
+        let buttonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteButtonAction(_:)))
+        buttonItem.tintColor = Palette.accent.uiColor
+        return buttonItem
+    }
+    
+    func setupShareButtonItem() -> UIBarButtonItem {
+        let buttonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareButtonAction(_:)))
+        buttonItem.tintColor = Palette.accent.uiColor
+        return buttonItem
+    }
+    
+    func setupSelectAllButtonItem() -> UIBarButtonItem {
+        let buttonItem = UIBarButtonItem(title: Locale.barButtonItemSelectAll.localized, style: .plain, target: self, action: #selector(selectAllButtonAction(_:)))
+        buttonItem.tintColor = Palette.accent.uiColor
+        return buttonItem
     }
     
 }
