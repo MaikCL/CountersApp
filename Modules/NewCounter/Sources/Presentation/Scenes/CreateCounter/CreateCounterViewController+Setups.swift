36ed878ec97d29a19a55ca3422d105b107b424e4
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-09 21:53:09 -0400

    Improve CreateCounterView

--- Modules/NewCounter/Sources/Presentation/Scenes/CreateCounter/CreateCounterViewController+Setups.swift
+++ Modules/NewCounter/Sources/Presentation/Scenes/CreateCounter/CreateCounterViewController+Setups.swift
@@ -8,6 +8,34 @@ extension CreateCounterViewController {
         let backItem = UIBarButtonItem()
         backItem.title = Locale.navigationBack.localized
         navigationItem.largeTitleDisplayMode = .never
+        
+        setupSaveBarButtonItem()
+        setupCancelBarButtonItem()
+    }
+    
+    func setupDelegates() {
+        innerView.delegate = self
+    }
+    
+    func setupSaveBarButtonItem() {
+        let saveBarButtonItem = UIBarButtonItem()
+        saveBarButtonItem.title = Locale.buttonItemSave.localized
+        saveBarButtonItem.isEnabled = false
+        saveBarButtonItem.tintColor = Palette.accent.uiColor
+        saveBarButtonItem.style = .done
+        saveBarButtonItem.target = self
+        saveBarButtonItem.action = #selector(saveNewCounter(_:))
+        navigationItem.rightBarButtonItem = saveBarButtonItem
+    }
+        
+    func setupCancelBarButtonItem() {
+        let cancelBarButtonItem = UIBarButtonItem()
+        cancelBarButtonItem.title = Locale.buttonItemCancel.localized
+        cancelBarButtonItem.tintColor = Palette.accent.uiColor
+        cancelBarButtonItem.style = .plain
+        cancelBarButtonItem.target = self
+        cancelBarButtonItem.action = #selector(cancelNewCounter(_:))
+        navigationItem.leftBarButtonItem = cancelBarButtonItem
     }
     
 }
