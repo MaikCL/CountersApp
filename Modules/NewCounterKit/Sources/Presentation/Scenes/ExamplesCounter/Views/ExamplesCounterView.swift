06f84a61d8dfbd0410297a115270e4ff500bc512
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-11 02:29:43 -0400

    Make and customization of Collectionview

--- Modules/NewCounterKit/Sources/Presentation/Scenes/ExamplesCounter/Views/ExamplesCounterView.swift
+++ Modules/NewCounterKit/Sources/Presentation/Scenes/ExamplesCounter/Views/ExamplesCounterView.swift
@@ -17,6 +17,10 @@ final class ExamplesCounterView: UIView {
        setupTopMessageLabel()
     }()
     
+    private lazy var collectionView: UICollectionView = {
+       setupCollectionView()
+    }()
+    
     init() {
         super.init(frame: .zero)
         setupView()
@@ -39,21 +43,30 @@ extension ExamplesCounterView {
     func setSubViews() {
         addSubview(topMessageView)
         addSubview(topMessageLabel)
+        addSubview(collectionView)
         setSubviewForAutoLayout(topMessageView)
         setSubviewForAutoLayout(topMessageLabel)
+        setSubviewForAutoLayout(collectionView)
     }
     
     func setupConstraint() {
         NSLayoutConstraint.activate([
             topMessageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            topMessageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            topMessageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            topMessageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            topMessageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             topMessageView.heightAnchor.constraint(equalToConstant: TopMessageViewConstants.height)
         ])
         
         NSLayoutConstraint.activate([
             topMessageLabel.centerYAnchor.constraint(equalTo: topMessageView.centerYAnchor),
             topMessageLabel.centerXAnchor.constraint(equalTo: topMessageView.centerXAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topMessageView.bottomAnchor, constant: 2),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
         ])
     }
     
@@ -89,5 +102,59 @@ private extension ExamplesCounterView {
         label.font = Font.topMessage
         return label
     }
+    
+    func setupCollectionView() -> UICollectionView {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+        collectionView.backgroundColor = Palette.background.uiColor
+        collectionView.alwaysBounceVertical = false
+        collectionView.register(SectionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SectionHeaderView.identifier)
+        return collectionView
+    }
+    
+}
+
+private extension ExamplesCounterView {
+    
+    func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        enum Constants {
+            static let headerWidth: CGFloat = 1.0
+            static let headerHeight: CGFloat = 60
+            static let height: CGFloat = 55
+            static let width: CGFloat = 150
+            static let insetTop: CGFloat = 5
+            static let insetBottom: CGFloat = 5
+            static let insetLeading: CGFloat = 5
+            static let insetTrailing: CGFloat = 5
+            static let groupSpacing: CGFloat = 5
+            static let edgeSpacing: CGFloat = 16
+        }
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Constants.headerWidth), heightDimension: .estimated(Constants.headerHeight))
+        let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        headerElement.extendsBoundary = true
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(Constants.width), heightDimension: .absolute(Constants.height))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.edgeSpacing = NSCollectionLayoutEdgeSpacing(leading: .fixed(Constants.edgeSpacing),
+                                                         top: .none,
+                                                         trailing: .fixed(Constants.edgeSpacing),
+                                                         bottom: .none)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(Constants.width), heightDimension: .absolute(Constants.height))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: Constants.insetTop,
+                                                        leading: Constants.insetLeading,
+                                                        bottom: Constants.insetBottom,
+                                                        trailing: Constants.insetBottom)
+        
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = Constants.groupSpacing
+        section.boundarySupplementaryItems = [headerElement]
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
     
 }
