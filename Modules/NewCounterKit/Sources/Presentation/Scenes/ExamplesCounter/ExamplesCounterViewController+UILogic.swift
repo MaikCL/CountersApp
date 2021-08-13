a3a8db2ee616e8faa6a57ae903cb7e5d2911fe8b
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-13 16:47:00 -0400

    Improve feedback in the ExampleCounter Scene

--- Modules/NewCounterKit/Sources/Presentation/Scenes/ExamplesCounter/ExamplesCounterViewController+UILogic.swift
+++ Modules/NewCounterKit/Sources/Presentation/Scenes/ExamplesCounter/ExamplesCounterViewController+UILogic.swift
@@ -2,6 +2,21 @@ import UIKit
 import DesignKit
 import AltairMDKCommon
 
+extension ExamplesCounterViewController {
+    
+    func showExceptionDialog(_ exception: Exception) {
+        let dismissAction = UIAlertAction(title: Locale.alertButtonDismiss.localized, style: .default) { _ in
+            self.dismissDialog()
+        }
+        let alert = UIAlertController(title: exception.errorTitle, message: exception.errorDescription, preferredStyle: .alert)
+        alert.view.tintColor = Palette.accent.uiColor
+        alert.addAction(dismissAction)
+        present(alert, animated: true)
+    }
+    
+}
+
+
 // MARK: Delegates
 
 extension ExamplesCounterViewController: ExamplesCounterViewCellDelegate {
