5399c5f0fe3851059e2e20b265b8537ce133b072
Miguel Angel Zapata <mzapatae@icloud.com>
2021-08-13 02:35:22 -0400

    Rewrite fetch logic in repo due bug
    - the bug caused the error screen not to be shown where it could not connect to the api (in cases where there was no local data)

--- Modules/CounterKit/Sources/Counter/Data/CounterRepository.swift
+++ Modules/CounterKit/Sources/Counter/Data/CounterRepository.swift
@@ -11,14 +11,13 @@ final class CounterRepository: CounterRepositoryProtocol {
     
     func fetchCounters() -> AnyPublisher<[Counter], Error> {
         return cloudSource.fetchCounters()
-            .delay(for: 0.1, scheduler: RunLoop.main)
-            .catch { _ in self.localSource.fetchCounters() }
-            .handleEvents(receiveOutput: { counters in
-                self.localSource.saveCounters(counters: counters)
-                    .receive(on: DispatchQueue.global(qos: .background))
-                    .sink(receiveCompletion: { _ in }, receiveValue: { _ in })
-                    .store(in: &self.cancellables)
-            })
+            .delay(for: 0.2, scheduler: RunLoop.main)
+            .catch { _ in
+                self.localSource.fetchCounters().tryMap {
+                    if $0.isEmpty { throw CounterException.cantLoadCounters } else { return $0 }
+                }
+            }
+            .flatMap { self.localSource.saveCounters(counters: $0) }
             .eraseToAnyPublisher()
     }
     
